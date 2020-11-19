f0050776c8c37610cd0b76d6f11539276e37648e
shobhit <shobhitgupta907@gmail.com>
2020-11-20 01:01:59 +0530

    All Models done

--- reserway/accounts/models.py
+++ reserway/accounts/models.py
@@ -4,8 +4,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 class BookingAgent(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE,related_name='booking_agent')
+    credit_card=models.CharField(max_length=12)
     address=models.TextField(max_length=500,blank=False)
 
     def __str__(self):
-        return str(self.user.id)+"  "+self.user.first_name+" "+self.user.last_name
+        return str(self.user.id)+"  "+self.user.first_name+" "+self.user.last_name+self.user.username
 
--- reserway/bookings/models.py
+++ reserway/bookings/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
-
+from accounts.models import BookingAgent
 
 # Create your models here.
 class Train(models.Model):
@@ -32,9 +32,9 @@ class TrainSchedule(models.Model):
 
 class Passenger(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.TextField()
+    name = models.CharField(max_length=30)
     age = models.IntegerField(
-        validators=[MaxValueValidator(200), MinValueValidator(0)]
+        validators=[MaxValueValidator(200), MinValueValidator(1)]
     )
     gender = models.CharField(
         max_length=1, choices=[("M", "Male"), ("F", "Female"), ("O", "Other")]
@@ -45,26 +45,18 @@ class Passenger(models.Model):
 
 class Ticket (models.Model):
     ticketId = models.AutoField(primary_key=True)
-    pnrNumber = models.TextField()
-    agent = models.ForeignKey(BookingAgent, on_delete=models.CASCADE, blank = False, null = False)
-    passenger1 = models.ForeignKey(Passenger, on_delete=models.CASCADE, blank = False, null = True)
-    passenger2 = models.ForeignKey(Passenger, on_delete=models.CASCADE, blank = False, null = True)
-    passenger3 = models.ForeignKey(Passenger, on_delete=models.CASCADE, blank = False, null = True)
-    passenger4 = models.ForeignKey(Passenger, on_delete=models.CASCADE, blank = False, null = True)
-    passenger5 = models.ForeignKey(Passenger, on_delete=models.CASCADE, blank = False, null = True)
-    passenger6 = models.ForeignKey(Passenger, on_delete=models.CASCADE, blank = False, null = True)
+    pnrNumber = models.CharField(max_length=12)
+    booking_agent = models.ForeignKey(BookingAgent, on_delete=models.CASCADE, blank = False, null = False,related_name='tickets')
+    passenger1 = models.OneToOneField(Passenger, on_delete=models.CASCADE, blank = False, null = True,related_name='ticket1')
+    passenger2 = models.OneToOneField(Passenger, on_delete=models.CASCADE, blank = False, null = True,related_name='ticket2')
+    passenger3 = models.OneToOneField(Passenger, on_delete=models.CASCADE, blank = False, null = True,related_name='ticket3')
+    passenger4 = models.OneToOneField(Passenger, on_delete=models.CASCADE, blank = False, null = True,related_name='ticket4')
+    passenger5 = models.OneToOneField(Passenger, on_delete=models.CASCADE, blank = False, null = True,related_name='ticket5')
+    passenger6 = models.OneToOneField(Passenger, on_delete=models.CASCADE, blank = False, null = True,related_name='ticket6')
 
     def __str__ (self):
-        return self.ticketId + " " + self.pnrNumber
+        return str(self.ticketId) + " " + self.pnrNumber
 
-class BookingAgent (models.Model):
-    agentId = models.AutoField(primary_key=True)
-    name = models.TextField()
-    creditCardNumber = models.TextField()
-    address = models.TextField()
-
-    def __str__(self):
-        return self.agentId + " " + self.name
 
 class BookingStatus (models.Model):
     journey = models.ForeignKey (TrainSchedule, on_delete=models.CASCADE, blank = False, null = False)
@@ -76,34 +68,34 @@ class BookingStatus (models.Model):
 
 class ACBookingStatus (models.Model):
     journey = models.ForeignKey (TrainSchedule, on_delete=models.CASCADE, blank = False, null = False)
-    coachNumber = models.TextField()
+    coachNumber = models.CharField(max_length=5)
     seatNumber = models.IntegerField()
-    pnrNumber = models.TextField()
+    ticket=models.ForeignKey(Ticket,on_delete=models.CASCADE,blank=False,null=False)
     passenger =  models.ForeignKey(Passenger, on_delete=models.CASCADE, blank = False, null = False)
 
     def __str__ (self):
-        return self.journey.journey_id + " " + self.coachNumber
+        return str(self.journey.journey_id) + " " + self.coachNumber
 
 class SleeperBookingStatus (models.Model):
     journey = models.ForeignKey (TrainSchedule, on_delete=models.CASCADE, blank = False, null = False)
-    coachNumber = models.TextField()
+    coachNumber = models.CharField(max_length=5)
     seatNumber = models.IntegerField()
-    pnrNumber = models.TextField()
+    ticket=models.ForeignKey(Ticket,on_delete=models.CASCADE,blank=False,null=False)
     passenger =  models.ForeignKey(Passenger, on_delete=models.CASCADE, blank = False, null = False)
 
     def __str__ (self):
-        return self.journey.journey_id + " " + self.coachNumber
+        return str(self.journey.journey_id) + " " + self.coachNumber
 
 class CoachStructureAC (models.Model):
-    seatNumber = models.AutoField(primary_key=True)
-    seatType = models.TextField()
+    seatNumber = models.IntegerField()
+    seatType = models.CharField(max_length=2)
 
     def __str__ (self):
-        return self.seatNumber + " " + self.seatType
+        return str(self.seatNumber) + " " + self.seatType
 
 class CoachStructureSleeper (models.Model):
-    seatNumber = models.AutoField(primary_key=True)
-    seatType = models.TextField()
+    seatNumber = models.IntegerField()
+    seatType = models.CharField(max_length=2)
 
     def __str__ (self):
-        return self.seatNumber + " " + self.seatType
+        return str(self.seatNumber) + " " + self.seatType
