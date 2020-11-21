238332df915d647ccb552c134f7eb6b8e717b532
Shubham1427 <shubhamsingh14272000@gmail.com>
2020-11-21 17:41:53 +0530

    Passengers Form Added

---
+++ reserway/bookings/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+
+class PassengerDetailsForm(forms.Form):
+    passenger1Name = forms.CharField(label='Passenger 1 Name:', max_length=100)
+    passenger1Age = forms.IntegerField(label='Pasenger 1 Age:', max_value=200, min_value=0)
+    passenger1Gender = forms.ChoiceField(label="Passenger 1 Gender:", choices=[("M", "Male"), ("F", "Female"), ("O", "Other")])
+    passenger2Name = forms.CharField(label='Passenger 2 Name:', max_length=100, required=False)
+    passenger2Age = forms.IntegerField(label='Pasenger 2 Age:', max_value=200, min_value=0, required=False)
+    passenger2Gender = forms.ChoiceField(label="Passenger 2 Gender:", choices=[("M", "Male"), ("F", "Female"), ("O", "Other")], required=False)
+    passenger3Name = forms.CharField(label='Passenger 3 Name:', max_length=100, required=False)
+    passenger3Age = forms.IntegerField(label='Pasenger 3 Age:', max_value=200, min_value=0, required=False)
+    passenger3Gender = forms.ChoiceField(label="Passenger 3 Gender:", choices=[("M", "Male"), ("F", "Female"), ("O", "Other")], required=False)
+    passenger4Name = forms.CharField(label='Passenger 4 Name:', max_length=100, required=False)
+    passenger4Age = forms.IntegerField(label='Pasenger 4 Age:', max_value=200, min_value=0, required=False)
+    passenger4Gender = forms.ChoiceField(label="Passenger 4 Gender:", choices=[("M", "Male"), ("F", "Female"), ("O", "Other")], required=False)
+    passenger5Name = forms.CharField(label='Passenger 5 Name:', max_length=100, required=False)
+    passenger5Age = forms.IntegerField(label='Pasenger 5 Age:', max_value=200, min_value=0, required=False)
+    passenger5Gender = forms.ChoiceField(label="Passenger 5 Gender:", choices=[("M", "Male"), ("F", "Female"), ("O", "Other")], required=False)
+    passenger6Name = forms.CharField(label='Passenger 6 Name:', max_length=100, required=False)
+    passenger6Age = forms.IntegerField(label='Pasenger 6 Age:', max_value=200, min_value=0, required=False)
+    passenger6Gender = forms.ChoiceField(label="Passenger 6 Gender:", choices=[("M", "Male"), ("F", "Female"), ("O", "Other")], required=False)
--- reserway/bookings/views.py
+++ reserway/bookings/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
+from .forms import PassengerDetailsForm
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
 def home(request):
     return render(request,'bookings/home.html')
+
+
+def PassengerDetailsFormPage(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = PassengerDetailsForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = PassengerDetailsForm()
+
+    return render(request, 'bookings/passenger_details_form.html', {'form': form})
