091649499dc62a7ccef102c182c5d279531ffdd7
Shubham1427 <shubhamsingh14272000@gmail.com>
2020-11-23 16:54:54 +0530

    Passengers form updated

--- reserway/bookings/views.py
+++ reserway/bookings/views.py
@@ -15,9 +15,6 @@ def PassengerDetailsFormPage(request):
         form = PassengerDetailsForm(request.POST)
         # check whether it's valid:
         if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
             return HttpResponseRedirect('')
 
     # if a GET (or any other method) we'll create a blank form
