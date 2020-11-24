8fd855eb7d028d622feac999e8a2273cf7e1a867
Shubham1427 <shubhamsingh14272000@gmail.com>
2020-11-24 22:42:28 +0530

    Search Results added

--- reserway/f_search/views.py
+++ reserway/f_search/views.py
@@ -24,11 +24,11 @@ def search_direct(s,d,flag,min_time=datetime.datetime(1,1,1)):
             print("Element=")
             print(element1,element2)
             if flag==0 and element1.journey.j_id==element2.journey.j_id and element1.stop_number<element2.stop_number:
-                t4.append(element1.journey)
+                t4.append((element1, element2))
             else:
                 print("Rejected1")
             if flag==1 and element1.journey.j_id==element2.journey.j_id and element1.stop_number<element2.stop_number and element1.departure_time>min_time:
-                t4.append(element1.journey)
+                t4.append((element1, element2))
             else:
                 print("Rejected2")
     # print("Results:")
@@ -64,10 +64,15 @@ def DisplaySearchForm(request):
             # # print("Results:")
             # # for element in t4:
             # #     print(element.j_id,element.date,element.train_name)
+            context = {}
             t4=search_direct(s,d,0)
+            context['direct_routes'] = []
             print("Single:")
+            i = 1
             for element in t4:
-                print(element.j_id,element.date,element.train_name)
+                if (element != None):
+                    context['direct_routes'].append((i, element[0], element[1]))
+                    print(element[0].journey.j_id,element[0].journey.date,element[0].journey.train_name)
 
             so2=Routes.objects.raw('SELECT * from f_search_routes')
             do2=Routes.objects.raw('SELECT * from f_search_routes')
@@ -92,22 +97,26 @@ def DisplaySearchForm(request):
                 so4=Routes.objects.raw('SELECT * from f_search_routes')
                 for item in so4:
                     if item.journey.j_id==jour and item.stop_number>sno:
-                        so5.append(item)
+                        so5.append((element, item))
             print("This:")
             for element in so5:
-                print(element.station_name,element.arrival_time)
+                print(element[1].station_name,element[1].arrival_time)
             t5=[]
             for element in so5:
                 print("Called")
-                temp=search_direct(element.station_name,d,1,element.arrival_time)
+                temp=search_direct(element[1].station_name,d,1,element[1].arrival_time)
                 if len(temp)>0:
-                    nl=[element,temp]
+                    nl=[element[0], element[1], temp]
                     t5.append(nl)
+            i = 1
             print("Here")
+            context['break_routes'] = []
             for item in t5:
-                for over_item in item[1]:
-                    if item[0].journey.j_id!=over_item.j_id:
-                        print(s,item[0].journey.train_name,item[0].station_name,over_item.train_name,d)
+                for over_item in item[2]:
+                    if item[1].journey.j_id!=over_item[1].journey.j_id:
+                        context['break_routes'].append((i, item[0], item[1], over_item[0], over_item[1]))
+                        print(s,item[1].journey.train_name,item[1].station_name,over_item[1].journey.train_name,d)
+            return render(request, 'f_search/results.html', context)
     else:
         form=SearchForm()
 
